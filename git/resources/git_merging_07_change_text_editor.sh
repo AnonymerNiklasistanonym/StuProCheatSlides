080e7dde61add499f786995d932c893b3c44b507
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2019-04-14 15:13:32 +0200

    Change text editor, use text editor for commit message

---
+++ git/resources/git_merging_07_change_text_editor.sh
@@ -0,0 +1,2 @@
+git config core.editor "vim" # VSCODE: "code --wait", NANO: "nano -w"
+# OTHERS: https://swcarpentry.github.io/git-novice/02-setup/
--- git/resources/git_workflow_03_commit.sh
+++ git/resources/git_workflow_03_commit.sh
@@ -1,1 +1,2 @@
 git commit -m "Title" -m "Optional description"
+# If you only enter "git commit" a text editor will be opened to do this
