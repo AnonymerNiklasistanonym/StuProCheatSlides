2a8546f13b049f6192e5709d7d17a1c7aafcad61
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2019-07-05 19:18:45 +0200

    Add local origin reset and commit editing with rebase

---
+++ git/resources/git_rebase_04_example_remove_directory.sh
@@ -0,0 +1,7 @@
+git reset HEAD~
+# Then add/remove files and make commits
+# rm -rf someDirectoryWithBinaries
+# git add .
+# git commit -m "Old commit message"
+# If you are ready continue the rebase
+git rebase --continue
---
+++ git/resources/git_reset_to_remote.sh
@@ -0,0 +1,3 @@
+git fetch origin
+# You can also select a different branch
+git reset --hard origin/master
